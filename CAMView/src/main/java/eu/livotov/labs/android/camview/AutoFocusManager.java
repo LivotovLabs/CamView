def7e47eb6cf17fb50d3cfc7458dc349a1f2661c
Dmitri Livotov <dmitri@livotov.eu>
2015-04-14 17:40:48 +0300

    language level 1.7/1.8 compat fix

--- CAMView/src/main/java/eu/livotov/labs/android/camview/AutoFocusManager.java
+++ CAMView/src/main/java/eu/livotov/labs/android/camview/AutoFocusManager.java
@@ -1,107 +1,109 @@
-package eu.livotov.labs.android.camview;
+/*
+ * Copyright (C) 2012 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import android.content.Context;
-import android.hardware.Camera;
-import android.util.Log;
+package eu.livotov.labs.android.camview;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+import android.content.Context;
+import android.hardware.Camera;
+import android.os.AsyncTask;
+import android.util.Log;
 
-final class AutoFocusManager implements Camera.AutoFocusCallback
-{
+
+
+final class AutoFocusManager implements Camera.AutoFocusCallback {
 
     private static final String TAG = AutoFocusManager.class.getSimpleName();
 
     private static final long AUTO_FOCUS_INTERVAL_MS = 2000L;
     private static final Collection<String> FOCUS_MODES_CALLING_AF;
-
-    static
-    {
+    static {
         FOCUS_MODES_CALLING_AF = new ArrayList<String>(2);
         FOCUS_MODES_CALLING_AF.add(Camera.Parameters.FOCUS_MODE_AUTO);
         FOCUS_MODES_CALLING_AF.add(Camera.Parameters.FOCUS_MODE_MACRO);
     }
 
+    private boolean active;
     private final boolean useAutoFocus;
     private final Camera camera;
-    private boolean active;
-    private CAMViewAsyncTask<?, ?, ?> outstandingTask;
+    private AsyncTask<Object, Object, Object> outstandingTask;
 
-    AutoFocusManager(Context context, Camera camera)
-    {
+    AutoFocusManager(Context context, Camera camera) {
         this.camera = camera;
+        //SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         String currentFocusMode = camera.getParameters().getFocusMode();
-        useAutoFocus = FOCUS_MODES_CALLING_AF.contains(currentFocusMode);
-        Log.d(TAG, "Current focus mode '" + currentFocusMode + "'; use auto focus? " + useAutoFocus);
+        useAutoFocus =
+                //sharedPrefs.getBoolean(PreferencesActivity.KEY_AUTO_FOCUS, true)
+                true
+                        &&
+                        FOCUS_MODES_CALLING_AF.contains(currentFocusMode);
+        Log.i(TAG, "Current focus mode '" + currentFocusMode + "'; use auto focus? " + useAutoFocus);
         start();
     }
 
     @SuppressWarnings("unchecked")
     @Override
-    public synchronized void onAutoFocus(boolean success, Camera theCamera)
-    {
-        if (active)
-        {
+    public synchronized void onAutoFocus(boolean success, Camera theCamera) {
+        if (active) {
             outstandingTask = new AutoFocusTask();
-            outstandingTask.execPool();
+            //new AsyncTaskExecManager().build().execute(outstandingTask);
+            outstandingTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         }
     }
 
-    synchronized void start()
-    {
-        if (useAutoFocus)
-        {
+    synchronized void start() {
+        if (useAutoFocus) {
             active = true;
-            try
-            {
+            try {
                 camera.autoFocus(this);
-            } catch (Throwable re)
-            {
+            } catch (RuntimeException re) {
+                // Have heard RuntimeException reported in Android 4.0.x+; continue?
                 Log.w(TAG, "Unexpected exception while focusing", re);
             }
         }
     }
 
-    synchronized void stop()
-    {
-        if (useAutoFocus)
-        {
-            try
-            {
+    synchronized void stop() {
+        if (useAutoFocus) {
+            try {
                 camera.cancelAutoFocus();
-            } catch (Throwable re)
-            {
+            } catch (RuntimeException re) {
+                // Have heard RuntimeException reported in Android 4.0.x+; continue?
                 Log.w(TAG, "Unexpected exception while cancelling focusing", re);
             }
         }
-
-        if (outstandingTask != null)
-        {
-            outstandingTask.cancel();
+        if (outstandingTask != null) {
+            outstandingTask.cancel(true);
             outstandingTask = null;
         }
         active = false;
     }
 
-
-    private final class AutoFocusTask extends CAMViewAsyncTask<Object, Object, Object>
-    {
-
+    private final class AutoFocusTask extends AsyncTask<Object,Object,Object> {
         @Override
-        protected Object doInBackground(Object... voids)
-        {
-            try
-            {
+        protected Object doInBackground(Object... voids) {
+            try {
                 Thread.sleep(AUTO_FOCUS_INTERVAL_MS);
-            } catch (InterruptedException ignored)
-            {
+            } catch (InterruptedException e) {
+                // continue
             }
-
-            synchronized (AutoFocusManager.this)
-            {
-                if (active)
-                {
+            synchronized (AutoFocusManager.this) {
+                if (active) {
                     start();
                 }
             }
