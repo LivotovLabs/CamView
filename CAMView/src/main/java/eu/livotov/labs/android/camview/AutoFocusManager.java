732e2e87e511284a0b5d55dd48627676582cf91f
Dmitri Livotov <dmitri@livotov.eu>
2015-06-26 09:21:36 +0300

    #3

--- CAMView/src/main/java/eu/livotov/labs/android/camview/AutoFocusManager.java
+++ CAMView/src/main/java/eu/livotov/labs/android/camview/AutoFocusManager.java
@@ -16,23 +16,25 @@
 
 package eu.livotov.labs.android.camview;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import android.content.Context;
 import android.hardware.Camera;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Collection;
 
 
-final class AutoFocusManager implements Camera.AutoFocusCallback {
+final class AutoFocusManager implements Camera.AutoFocusCallback
+{
 
     private static final String TAG = AutoFocusManager.class.getSimpleName();
 
     private static final long AUTO_FOCUS_INTERVAL_MS = 2000L;
     private static final Collection<String> FOCUS_MODES_CALLING_AF;
-    static {
+
+    static
+    {
         FOCUS_MODES_CALLING_AF = new ArrayList<String>(2);
         FOCUS_MODES_CALLING_AF.add(Camera.Parameters.FOCUS_MODE_AUTO);
         FOCUS_MODES_CALLING_AF.add(Camera.Parameters.FOCUS_MODE_MACRO);
@@ -41,69 +43,83 @@ final class AutoFocusManager implements Camera.AutoFocusCallback {
     private boolean active;
     private final boolean useAutoFocus;
     private final Camera camera;
-    private AsyncTask<Object, Object, Object> outstandingTask;
+    private CAMViewAsyncTask<Object, Object, Object> outstandingTask;
 
-    AutoFocusManager(Context context, Camera camera) {
+    AutoFocusManager(Context context, Camera camera)
+    {
         this.camera = camera;
-        //SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         String currentFocusMode = camera.getParameters().getFocusMode();
-        useAutoFocus =
-                //sharedPrefs.getBoolean(PreferencesActivity.KEY_AUTO_FOCUS, true)
-                true
-                        &&
-                        FOCUS_MODES_CALLING_AF.contains(currentFocusMode);
-        Log.i(TAG, "Current focus mode '" + currentFocusMode + "'; use auto focus? " + useAutoFocus);
+        useAutoFocus = true && FOCUS_MODES_CALLING_AF.contains(currentFocusMode);
         start();
     }
 
     @SuppressWarnings("unchecked")
     @Override
-    public synchronized void onAutoFocus(boolean success, Camera theCamera) {
-        if (active) {
+    public synchronized void onAutoFocus(boolean success, Camera theCamera)
+    {
+        if (active)
+        {
             outstandingTask = new AutoFocusTask();
-            //new AsyncTaskExecManager().build().execute(outstandingTask);
-            outstandingTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+            outstandingTask.execSerial();
         }
     }
 
-    synchronized void start() {
-        if (useAutoFocus) {
+    synchronized void start()
+    {
+        if (useAutoFocus)
+        {
             active = true;
-            try {
+            try
+            {
                 camera.autoFocus(this);
-            } catch (RuntimeException re) {
+            }
+            catch (RuntimeException re)
+            {
                 // Have heard RuntimeException reported in Android 4.0.x+; continue?
                 Log.w(TAG, "Unexpected exception while focusing", re);
             }
         }
     }
 
-    synchronized void stop() {
-        if (useAutoFocus) {
-            try {
+    synchronized void stop()
+    {
+        if (useAutoFocus)
+        {
+            try
+            {
                 camera.cancelAutoFocus();
-            } catch (RuntimeException re) {
+            }
+            catch (RuntimeException re)
+            {
                 // Have heard RuntimeException reported in Android 4.0.x+; continue?
                 Log.w(TAG, "Unexpected exception while cancelling focusing", re);
             }
         }
-        if (outstandingTask != null) {
-            outstandingTask.cancel(true);
+        if (outstandingTask != null)
+        {
+            outstandingTask.cancel();
             outstandingTask = null;
         }
         active = false;
     }
 
-    private final class AutoFocusTask extends AsyncTask<Object,Object,Object> {
+    private final class AutoFocusTask extends CAMViewAsyncTask<Object, Object, Object>
+    {
         @Override
-        protected Object doInBackground(Object... voids) {
-            try {
+        protected Object doInBackground(Object... voids)
+        {
+            try
+            {
                 Thread.sleep(AUTO_FOCUS_INTERVAL_MS);
-            } catch (InterruptedException e) {
+            }
+            catch (InterruptedException e)
+            {
                 // continue
             }
-            synchronized (AutoFocusManager.this) {
-                if (active) {
+            synchronized (AutoFocusManager.this)
+            {
+                if (active)
+                {
                     start();
                 }
             }
